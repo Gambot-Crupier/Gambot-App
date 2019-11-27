65add97aa814e128ad93e9b1d63e5373e6e450c3
Marina Joranhezon <mjoranhezon@gmail.com>
2019-11-27 14:15:56 -0200

    Create conditional on round page

--- lib/pages/round.dart
+++ lib/pages/round.dart
@@ -41,93 +41,133 @@ class _RoundPageState extends State<Round> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background_login.jpg"),
-            fit: BoxFit.cover,
-            colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+    if(Global.playerId == Global.playerTurnId){
+      return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background_login.jpg"),
+              fit: BoxFit.cover,
+              colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+            ),
+          ),
+          padding: EdgeInsets.only(top: 130, left: 40, right: 40),
+          child: ListView(
+            children: <Widget>[
+              Divider(),
+              Container(
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: new BorderRadius.circular(16.0)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Divider(),
+                    DefaultTextStyle(
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(color: Colors.black, fontSize: 20),
+                      child: Text(
+                        'A RODADA ESTÁ EM'
+                      )
+                    ),
+                    Divider(),
+                    DefaultTextStyle(
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+                      child: Text(
+                        Global.roundBet.toString()
+                      )
+                    ),
+                    Divider(),
+                    SizedBox(
+                      width: 160,
+                      height: 50, 
+                      child: DefaultButton(
+                        text: 'Aumentar', 
+                        fontSize: 20.0, 
+                        fontColor: Colors.white,
+                        backgroundColor: Colors.green,
+                        func: () {
+
+                        },
+                      ),
+                    ),
+                    Divider(),
+                    SizedBox(
+                      width: 160,
+                      height: 50,
+                      child: DefaultButton(
+                        text: 'Pagar', 
+                        fontSize: 20.0, 
+                          fontColor: Colors.white,
+                          backgroundColor: Colors.blue,
+                          func: () {
+                            payBet();
+                          },
+                      ),
+                    ),
+                    Divider(),
+                    SizedBox(
+                      width: 160,
+                      height: 50,
+                      child: DefaultButton(
+                        text: 'Fugir', 
+                        fontSize: 20.0, 
+                          fontColor: Colors.white,
+                          backgroundColor: Colors.red,
+                          func: () {
+                            leaveMatch();
+                          },
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        padding: EdgeInsets.only(top: 130, left: 40, right: 40),
-        child: ListView(
-          children: <Widget>[
-            Divider(),
-            Container(
-              decoration: new BoxDecoration(
-                color: Colors.white,
-                borderRadius: new BorderRadius.circular(16.0)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Divider(),
-                  DefaultTextStyle(
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(color: Colors.black, fontSize: 20),
-                    child: Text(
-                      'A RODADA ESTÁ EM'
-                    )
-                  ),
-                  Divider(),
-                  DefaultTextStyle(
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
-                    child: Text(
-                      Global.roundBet.toString()
-                    )
-                  ),
-                  Divider(),
-                  SizedBox(
-                    width: 160,
-                    height: 50, 
-                    child: DefaultButton(
-                      text: 'Aumentar', 
-                      fontSize: 20.0, 
-                      fontColor: Colors.white,
-                      backgroundColor: Colors.green,
-                      func: () {
-
-                      },
-                    ),
-                  ),
-                  Divider(),
-                  SizedBox(
-                    width: 160,
-                    height: 50,
-                    child: DefaultButton(
-                      text: 'Pagar', 
-                      fontSize: 20.0, 
-                        fontColor: Colors.white,
-                        backgroundColor: Colors.blue,
-                        func: () {
-                          payBet();
-                        },
-                    ),
-                  ),
-                  Divider(),
-                  SizedBox(
-                    width: 160,
-                    height: 50,
-                    child: DefaultButton(
-                      text: 'Fugir', 
-                      fontSize: 20.0, 
-                        fontColor: Colors.white,
-                        backgroundColor: Colors.red,
-                        func: () {
-                          leaveMatch();
-                        },
-                    ),
-                  ),
-                  Divider(),
-                ],
-              ),
+      );
+    } else {
+      return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background_login.jpg"),
+              fit: BoxFit.cover,
+              colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
             ),
-          ],
+          ),
+          padding: EdgeInsets.only(top: 130, left: 40, right: 40),
+          child: ListView(
+            children: <Widget>[
+              Divider(),
+              Container(
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: new BorderRadius.circular(16.0)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Divider(),
+                    DefaultTextStyle(
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(color: Colors.black, fontSize: 20),
+                      child: Text(
+                        'Espere a sua vez'
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
