8d54d9f3e5c68cd1b10c79363e44d4f90eea8282
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-12-02 22:39:39 -0300

    Adds new Icon

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <firebase_messaging/FirebaseMessagingPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseMessagingPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseMessagingPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
 }
 
 @end
